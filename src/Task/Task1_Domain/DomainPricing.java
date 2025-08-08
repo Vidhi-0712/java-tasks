0d15d20253701f0585ceabceb8f626c2592e42a6
Vidhisha <vidhishapatil777@gmail.com>
2025-08-08 12:49:23 +0530

    Task 1

---
+++ src/Task/Task1_Domain/DomainPricing.java
@@ -0,0 +1,23 @@
+package Task.Task1_Domain;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class DomainPricing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No. of Renewal Years");
+        int RenewalYears = sc.nextInt();
+        BigDecimal ry = BigDecimal.valueOf(RenewalYears);
+
+        CalculateDomainPricing obj = new CalculateDomainPricing();
+        obj.MarkupPrice();
+        obj.FinalConsumerPrice();
+        obj.PricePerSelectedYears(ry);
+        obj.MarkupForSelectedYears(RenewalYears);
+        obj.PricePerYearFinal(RenewalYears);
+        obj.FinalPriceWithRenewals(RenewalYears,ry);
+
+
+    }
+}
